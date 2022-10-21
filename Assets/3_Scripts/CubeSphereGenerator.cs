21b633ac6f27600b0d6a9b17b5fd487f26b3d69e
JGoodHub <jagoodmail@gmail.com>
2022-10-21 08:04:18 +0100

    commit

--- Assets/3_Scripts/CubeSphereGenerator.cs
+++ Assets/3_Scripts/CubeSphereGenerator.cs
@@ -1,25 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering;
 #if UNITY_EDITOR
 using UnityEditor;
 
 [CustomEditor(typeof(CubeSphereGenerator))]
 public class CubeSphereGeneratorInspector : Editor
 {
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
-        CubeSphereGenerator script = (CubeSphereGenerator)target;
+        CubeSphereGenerator script = (CubeSphereGenerator) target;
 
         if (GUILayout.Button("ClearFaces"))
             script.ClearFaces();
 
         if (GUILayout.Button("GenerateFaces"))
             script.GenerateFaces();
-
-
     }
 
 }
@@ -30,61 +31,133 @@ public class CubeSphereGenerator : MonoBehaviour
 {
 
     public Material _material;
-    public int _verticesPerBaseEdge;
+    public int _subDivisions;
+    public int _chunkDivisions;
 
     public float _radius = 5f;
 
+    public float _noiseScale;
+
+    public float Diameter => _radius * 2f;
+
     public void ClearFaces()
     {
-        foreach (Transform child in transform)
+        List<Transform> children = transform.Cast<Transform>().ToList();
+
+        foreach (Transform child in children)
             DestroyImmediate(child.gameObject);
     }
 
     public void GenerateFaces()
     {
-        GenerateCubeFace("FrontFace", gameObject, _material, Vector3.forward, Vector3.left, Vector3.up);
-        GenerateCubeFace("RearFace", gameObject, _material, Vector3.back, Vector3.right, Vector3.up);
+        ClearFaces();
 
-        GenerateCubeFace("RightFace", gameObject, _material, Vector3.right, Vector3.forward, Vector3.up);
-        GenerateCubeFace("LeftFace", gameObject, _material, Vector3.left, Vector3.back, Vector3.up);
+        _subDivisions = Mathf.Clamp(_subDivisions, 0, 12);
 
-        GenerateCubeFace("TopFace", gameObject, _material, Vector3.up, Vector3.right, Vector3.forward);
-        GenerateCubeFace("BottomFace", gameObject, _material, Vector3.down, Vector3.right, Vector3.back);
+        GenerateCubeFace("RearFace", gameObject, _material, Vector3.back, Vector3.right, Vector3.up, _subDivisions, _chunkDivisions);
+
+        GenerateCubeFace("FrontFace", gameObject, _material, Vector3.forward, Vector3.left, Vector3.up, _subDivisions, _chunkDivisions);
+
+        GenerateCubeFace("RightFace", gameObject, _material, Vector3.right, Vector3.forward, Vector3.up, _subDivisions, _chunkDivisions);
+        GenerateCubeFace("LeftFace", gameObject, _material, Vector3.left, Vector3.back, Vector3.up, _subDivisions, _chunkDivisions);
+
+        GenerateCubeFace("TopFace", gameObject, _material, Vector3.up, Vector3.right, Vector3.forward, _subDivisions, _chunkDivisions);
+        GenerateCubeFace("BottomFace", gameObject, _material, Vector3.down, Vector3.right, Vector3.back, _subDivisions, _chunkDivisions);
     }
 
-    private GameObject GenerateCubeFace(string faceName, GameObject parentGo, Material material, Vector3 normal, Vector3 rightUnityVector, Vector3 upUnitVector)
+    private GameObject GenerateCubeFace(string faceName, GameObject parentGo, Material material, Vector3 normal, Vector3 right, Vector3 up, int subDivisions, int chunkDivisions)
     {
-        GameObject faceGo = new GameObject(faceName);
-        faceGo.transform.parent = parentGo.transform;
+        GameObject rootGameObject = new GameObject(faceName);
+        rootGameObject.transform.parent = parentGo.transform;
 
-        MeshFilter meshFilter = faceGo.AddComponent<MeshFilter>();
-        MeshRenderer meshRenderer = faceGo.AddComponent<MeshRenderer>();
-        meshRenderer.sharedMaterial = material;
+        normal.Normalize();
+        right.Normalize();
+        up.Normalize();
 
-        Mesh faceMesh = new Mesh();
+        chunkDivisions = Mathf.Clamp(chunkDivisions, 0, subDivisions);
 
-        List<Vector3> vertices = new List<Vector3>();
+        int chunksPerAxis = Mathf.RoundToInt(Mathf.Pow(2, chunkDivisions));
+        float perChunkRadius = _radius / chunksPerAxis;
+        int perChunkSubDivisions = subDivisions - chunkDivisions;
+        int facesPerChunkAxis = Mathf.RoundToInt(Mathf.Pow(2, perChunkSubDivisions));
+        int verticesPerChunkAxis = facesPerChunkAxis + 1;
+        float vertexSpacing = (perChunkRadius * 2f) / facesPerChunkAxis;
 
-        vertices.Add((normal * _radius) - (rightUnityVector * _radius) + (upUnitVector * _radius));
-        vertices.Add((normal * _radius) + (rightUnityVector * _radius) + (upUnitVector * _radius));
-        vertices.Add((normal * _radius) - (rightUnityVector * _radius) - (upUnitVector * _radius));
-        vertices.Add((normal * _radius) + (rightUnityVector * _radius) - (upUnitVector * _radius));
+        Vector3 faceOrigin = normal * _radius;
+        Vector3 topLeftChunkPosition = faceOrigin - (right * _radius) + (up * _radius) + (right * perChunkRadius) - (up * perChunkRadius); // Represents the centre point of the top left chunk
 
-        List<int> triangles = new List<int>();
+        List<Vector3> chunkOffsets = new List<Vector3>();
+        for (int y = 0; y < chunksPerAxis; y++)
+            for (int x = 0; x < chunksPerAxis; x++)
+                chunkOffsets.Add(topLeftChunkPosition + (right * (x * perChunkRadius * 2f)) - (up * (y * perChunkRadius * 2f)));
 
-        triangles.AddRange(new int[] {0, 1, 2});
-        triangles.AddRange(new int[] {2, 1, 3});
+        Vector2Int statsCounter = new Vector2Int();
+        for (int index = 0; index < chunkOffsets.Count; index++)
+        {
+            Vector3 chunkOrigin = chunkOffsets[index];
+            Debug.DrawLine(Vector3.zero, chunkOrigin * 1.05f, Color.green, 5f);
 
-        faceMesh.SetVertices(vertices);
-        faceMesh.triangles = triangles.ToArray();
+            GameObject chunkGameObject = new GameObject($"{faceName}_Chunk_{index}");
+            chunkGameObject.transform.parent = rootGameObject.transform;
 
-        meshFilter.sharedMesh = faceMesh;
-        faceMesh.RecalculateNormals();
-        faceMesh.RecalculateBounds();
+            MeshFilter meshFilter = chunkGameObject.AddComponent<MeshFilter>();
+            MeshRenderer meshRenderer = chunkGameObject.AddComponent<MeshRenderer>();
+            meshRenderer.sharedMaterial = material;
 
-        Debug.DrawRay(Vector3.zero, vertices[0] * 2f, Color.cyan, 20f);
+            Mesh faceMesh = new Mesh();
+            faceMesh.name = $"{chunkGameObject.name}_Mesh";
+            faceMesh.indexFormat = perChunkSubDivisions <= 7 ? IndexFormat.UInt16 : IndexFormat.UInt32;
 
-        return faceGo;
+            // Vertices
+
+            List<Vector3> vertices = new List<Vector3>();
+
+            Vector3 cornerVertex = chunkOrigin - (right * perChunkRadius) + (up * perChunkRadius); // Represents the top left corner of the face
+
+            for (int y = 0; y < verticesPerChunkAxis; y++)
+                for (int x = 0; x < verticesPerChunkAxis; x++)
+                    vertices.Add(cornerVertex + (right * (x * vertexSpacing)) - (up * (y * vertexSpacing)));
+
+            // Triangles
+
+            List<int> triangles = new List<int>();
+
+            for (int i = 0; i < vertices.Count - verticesPerChunkAxis; i++)
+            {
+                if ((i + 1) % verticesPerChunkAxis == 0)
+                    continue;
+
+                triangles.AddRange(new[] {i, i + 1, i + verticesPerChunkAxis + 1});
+                triangles.AddRange(new[] {i, i + verticesPerChunkAxis + 1, i + verticesPerChunkAxis});
+            }
+
+            // Spherising & normals
+
+            List<Vector3> normals = new List<Vector3>();
+
+            for (int v = 0; v < vertices.Count; v++)
+            {
+                vertices[v] = vertices[v].normalized * (_radius + PerlinNoise.Noise(vertices[v] * _noiseScale));
+                normals.Add(vertices[v].normalized);
+            }
+
+            // Mesh building
+
+            faceMesh.SetVertices(vertices);
+            faceMesh.triangles = triangles.ToArray();
+            //faceMesh.SetNormals(normals);
+
+            meshFilter.sharedMesh = faceMesh;
+            faceMesh.RecalculateNormals();
+            faceMesh.RecalculateBounds();
+
+            statsCounter.x += vertices.Count;
+            statsCounter.y += triangles.Count / 3;
+        }
+
+        Debug.Log($"Created face with {statsCounter.x} vertices and {statsCounter.y} triangles");
+
+        return rootGameObject;
     }
 
 }
--- Assets/3_Scripts/StatsPanel.cs
+++ Assets/3_Scripts/StatsPanel.cs
@@ -42,7 +42,7 @@ public class StatsPanel : MonoBehaviour
 
         _apoapsisText.text = $"{apoapsisAltitude.ToString(CultureInfo.CurrentCulture).PadLeft(7, '0')}m";
         _periapsisText.text = $"{periapsisAltitude.ToString(CultureInfo.CurrentCulture).PadLeft(7, '0')}m";
-        _inclinationText.text = $"Inclination: {Mathf.Round(keplerOrbitElements.Inclination * 10f) / 10f}°";
+        _inclinationText.text = $"Inclination: {Mathf.Round(keplerOrbitElements.Inclination * 100f) / 100f}°";
         _orbitalPeriodText.text = $"Orbital Period: {SecondsToDateString(Mathf.RoundToInt(keplerOrbitElements.OrbitalPeriod))}";
     }
 
